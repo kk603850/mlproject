18fef3c36cac4e7d078fe286da282c8e22ceb3fd
Kundan Kumar <kk603850@gmail.com>
2025-07-27 15:30:38 +0530

    model train with hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import dill
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exceptions import CustomException
 
@@ -18,12 +19,18 @@ def save_obj(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(x_train,y_train,x_test,y_test,models):
+def evaluate_models(x_train,y_train,x_test,y_test,models,param):
     try:
         report={}
         for i in range(len(list(models))):
-            model=list(models.values())[i]
-            model.fit(x_train, y_train)
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
             y_train_pred=model.predict(x_train)
             y_test_pred=model.predict(x_test)
             train_model_score=r2_score(y_train,y_train_pred)
